dc96c60ff37eb7ce047ba149f33ee5afe596a766
ahmednss <ahmed.n.abdeltwab@gmail.com>
2022-10-14 23:50:55 +0200

    change function types

--- src/utilties/readDir.ts
+++ src/utilties/readDir.ts
@@ -1,10 +1,11 @@
 import { readdir } from 'node:fs/promises';
 
-export default async (path: string): Promise<string[] | undefined> => {
+export default async (path: string): Promise<string[] | null> => {
 	try {
 		const files = await readdir(path);
 		return files;
 	} catch (err) {
 		console.error(err);
+		return null;
 	}
 };
