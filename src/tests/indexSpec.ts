07687ef15f9f95f8895b9161caab10fee01850e2
ahmednss <ahmed.n.abdeltwab@gmail.com>
2022-10-14 18:53:28 +0200

    adding testing the route api/image

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -2,10 +2,19 @@ import supertest from 'supertest';
 import app from '../index';
 
 const request = supertest(app);
-describe('Test endpoint responses', () => {
-	it('gets the api endpoint', async (done) => {
-		const response = await request.get('/api/images');
-		expect(response.status).toBe(200);
-		done();
+describe('GET /api/images', () => {
+	it('should respond with json and status 404', (done: DoneFn) => {
+		request
+			.get('/api/images')
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(404)
+			.end(function (err) {
+				if (err) {
+					done.fail(err);
+				} else {
+					done();
+				}
+			});
 	});
 });
