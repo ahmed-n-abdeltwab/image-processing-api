417df7332fca7597dd6ba24dc35cbcfb19eed6ee
ahmednss <ahmed.n.abdeltwab@gmail.com>
2022-10-14 23:33:50 +0200

    adding types folder

---
+++ src/types/ImageOption.ts
@@ -0,0 +1,5 @@
+type ImageOption = {
+	width?: number;
+	height?: number;
+};
+export default ImageOption;
---
+++ src/types/Query.ts
@@ -0,0 +1,5 @@
+import ImageOption from './ImageOption';
+type Query = ImageOption & {
+	filename?: string;
+};
+export default Query;
